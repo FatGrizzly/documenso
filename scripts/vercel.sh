679154ff882e729d19bbb0aec4220bf0b2332987
Mythie <me@lucasjamessmith.me>
2023-09-17 06:47:50 +0000

    fix: improve script and handle seeding

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,52 +1,80 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit on error.
-set -e
+set -euo pipefail
 
-# Navigate to the root of the project.
-cd "$(dirname "$0")/../"
+# Get the directory of this script, regardless of where it is called from.
+SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
 
-# Function which runs the required build steps.
-build() {
-  echo "[VercelBuild]: Building $DEPLOYMENT_TARGET website."
 
-  npm run prisma:generate --workspace=@documenso/prisma
-  turbo run build
+function log() {
+  echo "[VercelBuild]: $1"
 }
 
+function build_webapp() {
+  log "Building webapp for $VERCEL_ENVIRONMENT"
+
+  npm run prisma:migrate-deploy --workspace=@documenso/prisma
+
+  if [[ "$VERCEL_ENVIRONMENT" != "production" ]]; then
+    log "Seed database for $VERCEL_ENVIRONMENT"
+
+    npm run prisma:seed --workspace=@documenso/prisma
+  fi
+
+  npm run build -- --filter @documenso/app
+}
+
+function remap_webapp_env() {
+  if [[ "$VERCEL_ENVIRONMENT" != "production" ]]; then
+    log "Remapping webapp environment variables for $VERCEL_ENVIRONMENT"
+
+    export NEXTAUTH_URL="https://$VERCEL_URL"
+    export NEXT_PUBLIC_WEBAPP_URL="https://$VERCEL_URL"
+  fi
+}
+
+function build_marketing() {
+  log "Building marketing for $VERCEL_ENVIRONMENT"
+
+  npm run prisma:generate --workspace=@documenso/prisma
+  npm run build -- --filter @documenso/marketing
+}
+
+function remap_marketing_env() {
+  if [[ "$VERCEL_ENVIRONMENT" != "production" ]]; then
+    log "Remapping marketing environment variables for $VERCEL_ENVIRONMENT"
+
+    export NEXT_PUBLIC_MARKETING_URL="https://$VERCEL_URL"
+  fi
+}
+
+function remap_supabase_integration() {
+  log "Remapping Supabase integration for $VERCEL_ENVIRONMENT"
+
+  export NEXT_PRIVATE_DATABASE_URL="$POSTGRES_URL"
+  export NEXT_PRIVATE_DIRECT_DATABASE_URL="$POSTGRES_URL_NON_POOLING"
+}
+
+# Navigate to the root of the project.
+cd "$SCRIPT_DIR/.."
+
 # Check if the script is running on Vercel.
-if [ -z "$VERCEL" ]; then
-  echo "[VercelBuild]: ERROR - This script must be run as part of the Vercel build process."
+if [[ -z "$VERCEL" ]]; then
+  log "ERROR - This script must be run as part of the Vercel build process."
   exit 1
 fi
 
-# Validate DEPLOYMENT_TARGET
-if [ "$DEPLOYMENT_TARGET" != "webapp" ] && [ "$DEPLOYMENT_TARGET" != "marketing" ]; then
-  echo "[VercelBuild]: ERROR - Missing or invalid DEPLOYMENT_TARGET environment variable."
-  echo "[VercelBuild]: ERROR - DEPLOYMENT_TARGET must be either 'webapp' or 'marketing'."
-  exit 1
-fi
-
-# Production builds do not need remapping.
-if [ "$VERCEL_ENV" = "production" ]; then
-  build
-  exit 0
-fi
-
-# Remap the NEXT_PUBLIC_WEBAPP_URL to the Vercel URL.
-if [ "$DEPLOYMENT_TARGET" = "webapp" ]; then
-  export NEXT_PUBLIC_WEBAPP_URL="https://$VERCEL_URL"
-fi
-
-# Remap the NEXT_PUBLIC_MARKETING_URL to the Vercel URL.
-if [ "$DEPLOYMENT_TARGET" = "marketing" ]; then
-  export NEXT_PUBLIC_MARKETING_URL="https://$VERCEL_URL"
-fi
-
-export NEXTAUTH_URL="$NEXT_PUBLIC_WEBAPP_URL"
-
-# Utilise Supabase environment variables if possible.
-export NEXT_PRIVATE_DATABASE_URL="${POSTGRES_PRISMA_URL:-$NEXT_PRIVATE_DATABASE_URL}"
-export NEXT_PRIVATE_DIRECT_DATABASE_URL="${POSTGRES_URL_NON_POOLING:-$NEXT_PRIVATE_DATABASE_URL}"
-
-build
+case "$DEPLOYMENT_TARGET" in
+  "webapp")
+    build_webapp
+    ;;
+  "marketing")
+    build_marketing
+    ;;
+  *)
+    log "ERROR - Missing or invalid DEPLOYMENT_TARGET environment variable."
+    log "ERROR - DEPLOYMENT_TARGET must be either 'webapp' or 'marketing'."
+    exit 1
+    ;;
+esac
