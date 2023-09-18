1df304dd0178e05478701aead82e2ebd9e4d4b19
Mythie <me@lucasjamessmith.me>
2023-09-18 00:46:01 +0000

    fix: pgbouncer support

--- scripts/remap-vercel-env.cjs
+++ scripts/remap-vercel-env.cjs
@@ -18,6 +18,16 @@ const remapVercelEnv = () => {
     process.env.NEXT_PRIVATE_DIRECT_DATABASE_URL = process.env.POSTGRES_URL_NON_POOLING;
   }
 
+  // If we're using a connection pool, we need to let Prisma know that
+  // we're using PgBouncer.
+  if (process.env.NEXT_PRIVATE_DATABASE_URL !== process.env.NEXT_PRIVATE_DIRECT_DATABASE_URL) {
+    const url = new URL(process.env.NEXT_PRIVATE_DATABASE_URL);
+
+    url.searchParams.set('pgbouncer', 'true');
+
+    process.env.NEXT_PRIVATE_DATABASE_URL = url.toString();
+  }
+
   if (process.env.VERCEL_ENV !== 'production' && process.env.DEPLOYMENT_TARGET === 'webapp') {
     process.env.NEXTAUTH_URL = `https://${process.env.VERCEL_URL}`;
     process.env.NEXT_PUBLIC_WEBAPP_URL = `https://${process.env.VERCEL_URL}`;
