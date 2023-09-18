b014199617f92cdaa5e3b1995d46cf7298ee8bce
Mythie <me@lucasjamessmith.me>
2023-09-18 04:09:41 +0000

    chore: env var shenanigans

--- packages/prisma/helper.ts
+++ packages/prisma/helper.ts
@@ -1,27 +1,17 @@
 /// <reference types="@documenso/tsconfig/process-env.d.ts" />
 
 export const getDatabaseUrl = () => {
-  if (process.env.NEXT_PRIVATE_DATABASE_URL) {
-    return process.env.NEXT_PRIVATE_DATABASE_URL;
-  }
-
   if (process.env.POSTGRES_URL) {
-    process.env.NEXT_PRIVATE_DATABASE_URL = process.env.POSTGRES_URL;
+    // If we're using a connection pool, we need to let Prisma know that
+    // we're using PgBouncer.
+    if (process.env.POSTGRES_URL !== process.env.POSTGRES_URL_NON_POOLING) {
+      const url = new URL(process.env.POSTGRES_URL);
+
+      url.searchParams.set('pgbouncer', 'true');
+
+      process.env.POSTGRES_URL = url.toString();
+    }
   }
 
-  if (process.env.POSTGRES_URL_NON_POOLING) {
-    process.env.NEXT_PRIVATE_DIRECT_DATABASE_URL = process.env.POSTGRES_URL_NON_POOLING;
-  }
-
-  // If we're using a connection pool, we need to let Prisma know that
-  // we're using PgBouncer.
-  if (process.env.NEXT_PRIVATE_DATABASE_URL !== process.env.NEXT_PRIVATE_DIRECT_DATABASE_URL) {
-    const url = new URL(process.env.NEXT_PRIVATE_DATABASE_URL);
-
-    url.searchParams.set('pgbouncer', 'true');
-
-    process.env.NEXT_PRIVATE_DATABASE_URL = url.toString();
-  }
-
-  return process.env.NEXT_PRIVATE_DATABASE_URL;
+  return process.env.POSTGRES_URL;
 };
